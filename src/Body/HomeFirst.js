9201405afa1cf8b15a172ce149c4abeac9b2f9bb
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2019-09-25 18:22:43 +0200

    refactor home first page with lessons

--- src/Body/HomeFirst.js
+++ src/Body/HomeFirst.js
@@ -1,123 +1,42 @@
 import React from 'react';
 import { Row, Col, Card, Badge } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import data from '../Data/HomeFirstLessons.json';
+
 
 function HomeFirst() {
+    let lekcije = data['lekcije']
+   
+   return  (<React.Fragment> 
+   
+   {lekcije.map(
+        (lekcija,index)=>{
+            let number = index*2+1;
+            return (<Row className="homearea mt-4">
+                <Col>
+                    <Link to={"/lekcija"+number} style={{ color: 'inherit' }}><Card>
+                        <Card.Body>
+                            <Card.Title>{lekcija[0].title} <Badge className="imelekcije" variant="light">Lekcija {number}</Badge></Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">{lekcija[0].subtitle}</Card.Subtitle>
+                        </Card.Body>
+                    </Card></Link>
+                </Col>
+                
+                <Col>
+                    <Link to={"/lekcija"+number+1} style={{ color: 'inherit' }}><Card>
+                        <Card.Body>
+                            <Card.Title>{lekcija[1].title} <Badge className="imelekcije" variant="light">Lekcija {number+1}</Badge></Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">{lekcija[1].subtitle}</Card.Subtitle>
+                        </Card.Body>
+                    </Card></Link>
+                </Col>
+            </Row>)
+          
+            }
+            )}
+            </React.Fragment>)
 
-    return <React.Fragment><Row className="homearea mt-4">
-            <Col>
-                <Link to="/lekcija1" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>VAKF <Badge className="imelekcije" variant="light">Lekcija 1</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Stajanje prilikom učenja</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-            <Col>
-                <Link to="/lekcija2" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>LAFZATULLAH <Badge className="imelekcije" variant="light">Lekcija 2</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Izgovaranje riječi Allah اللّٰه</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-        </Row>
-
-        <Row className="homearea mt-4">
-            <Col>
-                <Link to="/lekcija3" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IDGAM MISLEJN <Badge className="imelekcije" variant="light">Lekcija 3</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Uklapanje istih harfova</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-            <Col>
-                <Link to="/lekcija4" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IDGAM MISLEJN MEAL-GUNNEH <Badge className="imelekcije" variant="light">Lekcija 4</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Uklapanje istih harfova sa propuštanjem zraka kroz nos</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-        </Row>
-
-        <Row className="homearea mt-4">
-            <Col>
-                <Link to="/lekcija5" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IDGAM MISLEJN MEAL-GUNNEH <Badge className="imelekcije" variant="light">Lekcija 5</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Uklapanje istih harfova sa propuštanjem zraka kroz nos</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-            <Col>
-                <Link to="/lekcija6" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IDGAM BILA GUNNEH <Badge className="imelekcije" variant="light">Lekcija 6</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Uklapanje bez propuštanja zraka kroz nos</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-        </Row>
-
-        <Row className="homearea mt-4">
-            <Col>
-                <Link to="/lekcija7" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IDGAM BILA GUNNEH <Badge className="imelekcije" variant="light">Lekcija 7</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Uklapanje bez propuštanja zraka kroz nos</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-            <Col>
-                <Link to="/lekcija8" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IKLAB <Badge className="imelekcije" variant="light">Lekcija 8</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Pretvaranje harfa N (ن) u harf M (م)</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-        </Row>
-
-        <Row className="homearea mt-4">
-            <Col>
-                <Link to="/lekcija9" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IZHAR HALKIJJ <Badge className="imelekcije" variant="light">Lekcija 9</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Čisto izgovaranje harfa N (ن) ili tenvina</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-            <Col>
-                <Link to="/lekcija10" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IZHAR ŠEFEVIJJ <Badge className="imelekcije" variant="light">Lekcija 10</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Čisto izgovaranje harfa M (م)</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-        </Row>
-
-        <Row className="homearea mt-4">
-            <Col>
-                <Link to="/lekcija11" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IHFA <Badge className="imelekcije" variant="light">Lekcija 11</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Skrivanje harfa N (ن)</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-            <Col>
-                <Link to="/lekcija12" style={{ color: 'inherit' }}><Card>
-                    <Card.Body>
-                        <Card.Title>IHFA ŠEFEVIJJ <Badge className="imelekcije" variant="light">Lekcija 12</Badge></Card.Title>
-                        <Card.Subtitle className="mb-2 text-muted">Skrivanje harfa M (م)</Card.Subtitle>
-                    </Card.Body>
-                </Card></Link>
-            </Col>
-        </Row>
-        </React.Fragment>;
+      
 
 }
 
