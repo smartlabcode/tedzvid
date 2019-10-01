0bcfccc920f1a92f56e1cb72c61f95d67d2d955e
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2019-10-01 17:50:41 +0200

    ubaci 8. vjezbu

--- src/Lessons/Lesson7.js
+++ src/Lessons/Lesson7.js
@@ -5,54 +5,52 @@ import VjezbeRow from '../Helpers/VjezbeHelper';
 import Footer from '../Body/MainFooter';
 
 // Bootstrap
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 // Other
 import '../App.scss';
 
 function L7() {
-    return (
-        <React.Fragment>
-        <Row>
-          <Col>
-            <h2 className="text-center font-weight-bold text-uppercase">IDGAM BILA GUNNEH</h2>
-            <h4 className="text-center">uklapanje bez propuštanja zraka kroz nos</h4>
-            <hr/>
-          </Col>
-        </Row>
+	return (
+		<React.Fragment>
+			<Row>
+				<Col>
+					<h2 className="text-center font-weight-bold text-uppercase">IDGAM BILA GUNNEH</h2>
+					<h4 className="text-center">uklapanje bez propuštanja zraka kroz nos</h4>
+					<hr />
+				</Col>
+			</Row>
 
-        <Row>
+			<Row>
 				<Col>
 					<br />
 				</Col>
 			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          Kada poslije harfa N sa sukunom (نْ) ili tenvina EN  ــــــًــــــ, IN ـــــٍــــــ , UN  ــــــٌـــــ dođe harf L (ل) ili R (ر), dolazi do potpunog uklapanja harfa N (ن) u harf L (ل), odnosno, R (ر), tj. N (ن) se nikako ne uči, npr.:
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					Kada poslije harfa N sa sukunom (نْ) ili tenvina EN ــــــًــــــ, IN ـــــٍــــــ , UN ــــــٌـــــ
+					dođe harf L (ل) ili R (ر), dolazi do potpunog uklapanja harfa N (ن) u harf L (ل), odnosno, R (ر),
+					tj. N (ن) se nikako ne uči, npr.:
+				</Col>
+			</Row>
 
-        <Row>
-          <Col>
-            { PlayerRow(data, 'row1') }
-          </Col>
-        </Row>
+			<Row>
+				<Col>{PlayerRow(data, 'row1')}</Col>
+			</Row>
 
-        <Row>
-          <Col>
-            { VjezbeRow(data, 'multirow', 'word1') }
-            { PlayerRow(data, 'row2') }
-          </Col>
-        </Row>
+			<Row>
+				<Col>
+					{VjezbeRow(data, 'multirow', 'word1')}
+					{PlayerRow(data, 'row2')}
+				</Col>
+			</Row>
 
-        <Row>
-          <Col>
-            { VjezbeRow(data, 'multirow', 'word2') }
-          </Col>
-        </Row>
+			<Row>
+				<Col>{VjezbeRow(data, 'multirow', 'word2')}</Col>
+			</Row>
 
-        <Row>
+			<Row>
 				<Col>
 					<br />
 				</Col>
@@ -64,14 +62,14 @@ function L7() {
 				</Col>
 			</Row>
 
-        <h2 className="text-center">Vježbe</h2>
-        <hr/>
+			<h2 className="text-center">Vježbe</h2>
+			<hr />
 
 			<Row className="text-center">
 				<Col>
 					۞
-          {VjezbeRow(data, 'vjezba', 'broj9')}
-          ۞
+					{VjezbeRow(data, 'vjezba', 'broj9')}
+					۞
 					{VjezbeRow(data, 'vjezba', 'broj8')}
 				</Col>
 			</Row>
@@ -87,33 +85,32 @@ function L7() {
 
 			<Row className="text-center">
 				<Col>
-          ۞
-          {VjezbeRow(data, 'vjezba', 'broj13')}
 					۞
-          {VjezbeRow(data, 'vjezba', 'broj12')}
+					{VjezbeRow(data, 'vjezba', 'broj13')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj12')}
 				</Col>
 			</Row>
 			<Row className="text-center">
 				<Col>
 					۞
-          {VjezbeRow(data, 'vjezba', 'broj14')}
+					{VjezbeRow(data, 'vjezba', 'broj14')}
 				</Col>
 			</Row>
-      <Row className="text-center">
+			<Row className="text-center">
 				<Col>
 					۞
 					{VjezbeRow(data, 'vjezba', 'broj15')}
 				</Col>
 			</Row>
-      <Row className="text-center">
+			<Row className="text-center">
 				<Col>
 					۞
 					{VjezbeRow(data, 'vjezba', 'broj16')}
 				</Col>
 			</Row>
+		</React.Fragment>
+	);
+}
 
-        </React.Fragment>
-    );
-  }
-
-  export default L7;
+export default L7;
--- src/Lessons/Lesson8.js
+++ src/Lessons/Lesson8.js
@@ -1,64 +1,91 @@
 import React from 'react';
 import data from '../Data/L8Data.json';
 import PlayerRow from '../Helpers/PlayerHelper';
-//import VjezbeRow from '../Helpers/VjezbeHelper';
+import VjezbeRow from '../Helpers/VjezbeHelper';
 import Footer from '../Body/MainFooter';
 
 // Bootstrap
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 // Other
 import '../App.scss';
 
 function L8() {
-    return (
-        <React.Fragment>
-        <Row>
-          <Col>
-            <h2 className="text-center font-weight-bold text-uppercase">IKLAB</h2>
-            <h4 className="text-center">pretvaranje harfa N (ن) u harf M (م)</h4>
-            <hr/>
-          </Col>
-        </Row>
+	return (
+		<React.Fragment>
+			<Row>
+				<Col>
+					<h2 className="text-center font-weight-bold text-uppercase">IKLAB</h2>
+					<h4 className="text-center">pretvaranje harfa N (ن) u harf M (م)</h4>
+					<hr />
+				</Col>
+			</Row>
+			<Row>
+				<Col className="opisLekcije">
+					Kada poslije harfa N sa sukunom (نْ) ili tenvina EN ــــــًــــــ, IN ـــــٍــــــ , UN ــــــٌـــــ
+					dođe harf B (ب), onda se harf N (ن) pretvara u harf M (م) u trajanju od 2 hareketa:
+				</Col>
+			</Row>
+			<Row>
+				<Col>{PlayerRow(data, 'row1')}</Col>
+			</Row>
+			<Row>
+				<Col>{PlayerRow(data, 'row2')}</Col>
+			</Row>
+			<Row>
+				<Col>{PlayerRow(data, 'row3')}</Col>
+			</Row>
+			<h2 className="text-center">Vježbe</h2>
+			<hr />
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj10')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj9')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj8')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj12')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj11')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj14')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj13')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj16')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj15')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj17')}
+				</Col>
+			</Row>{' '}
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj18')}
+				</Col>
+			</Row>
+			<Footer prev="/lekcija7" next="/lekcija9" />
+		</React.Fragment>
+	);
+}
 
-        <Row>
-          <Col className="opisLekcije">
-          Kada poslije harfa N sa sukunom (نْ) ili tenvina EN  ــــــًــــــ, IN ـــــٍــــــ , UN  ــــــٌـــــ dođe harf B (ب), onda se harf N (ن) pretvara u harf M (م) u trajanju od 2 hareketa:
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            { PlayerRow(data, 'row1') }
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            { PlayerRow(data, 'row2') }
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            { PlayerRow(data, 'row3') }
-          </Col>
-        </Row>
-
-        <h2 className="text-center">Vježbe</h2>
-
-        <hr/>
-
-        <Row className="text-center">
-          <Col>
-            VJEŽBE 8
-          </Col>
-        </Row>
-
-        <Footer prev="/lekcija7" next="/lekcija9" />
-
-        </React.Fragment>
-    );
-  }
-
-  export default L8;
+export default L8;
