cf33cb4906714788395fecf3bb3e7c15d8be528a
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2019-10-01 18:35:40 +0200

    ubaci 10. vjezbu

--- src/Lessons/Lesson10.js
+++ src/Lessons/Lesson10.js
@@ -5,61 +5,82 @@ import VjezbeRow from '../Helpers/VjezbeHelper';
 import Footer from '../Body/MainFooter';
 
 // Bootstrap
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 // Other
 import '../App.scss';
 
 function L10() {
-    return (
-        <React.Fragment>
-        <Row>
-          <Col>
-            <h2 className="text-center font-weight-bold text-uppercase">IZHAR ŠEFEVIJJ</h2>
-            <h4 className="text-center">čisto izgovaranje harfa M (م)</h4>
-            <hr/>
-          </Col>
-        </Row>
+	return (
+		<React.Fragment>
+			<Row>
+				<Col>
+					<h2 className="text-center font-weight-bold text-uppercase">IZHAR ŠEFEVIJJ</h2>
+					<h4 className="text-center">čisto izgovaranje harfa M (م)</h4>
+					<hr />
+				</Col>
+			</Row>
+			<Row>
+				<Col className="opisLekcije">
+					Kada harf M sa sukunom (مْ) dođe ispred bilo kojeg harfa, osim harfova B (ب) i M (م), onda se harf M
+					(م) izgovara čisto, tj. bez uklapanja, npr.:
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					{VjezbeRow(data, 'multirow', 'word1')}
+					{PlayerRow(data, 'row1')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>{VjezbeRow(data, 'multirow', 'word2')}</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>{VjezbeRow(data, 'multirow', 'word3')}</Col>
+			</Row>
+			<h2 className="text-center">Vježbe</h2>
+			<hr />
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj8')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj7')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj6')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj10')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj9')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj11')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj14')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj12')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj13')}
+				</Col>
+			</Row>
+			<Footer prev="/lekcija9" next="/lekcija11" />
+		</React.Fragment>
+	);
+}
 
-        <Row>
-          <Col className="opisLekcije">
-          Kada harf M sa sukunom (مْ) dođe ispred bilo kojeg harfa, osim harfova B (ب) i M (م), onda se harf  M (م) izgovara čisto, tj. bez uklapanja, npr.:
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            { VjezbeRow(data, 'multirow', 'word1') }
-            { PlayerRow(data, 'row1') }
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            { VjezbeRow(data, 'multirow', 'word2') }
-          </Col>
-        </Row>
-
-        <Row>
-          <Col>
-            { VjezbeRow(data, 'multirow', 'word3') }
-          </Col>
-        </Row>
-
-        <h2 className="text-center">Vježbe</h2>
-
-        <hr/>
-
-        <Row className="text-center">
-          <Col>
-            VJEŽBE 10
-          </Col>
-        </Row>
-
-        <Footer prev="/lekcija9" next="/lekcija11" />
-
-        </React.Fragment>
-    );
-  }
-
-  export default L10;
+export default L10;
