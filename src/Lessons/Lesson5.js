5495e4dcbbaa9e501752d2cfb0095e444819303a
brodude456 <fatihefeememisevic@gmail.com>
2019-10-01 16:54:49 +0200

    vjezba broj 5

--- src/Lessons/Lesson5.js
+++ src/Lessons/Lesson5.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import data from '../Data/L5Data.json';
 import PlayerRow from '../Helpers/PlayerHelper';
-//import VjezbeRow from '../Helpers/VjezbeHelper';
+import VjezbeRow from '../Helpers/VjezbeHelper';
 import Footer from '../Body/MainFooter';
 
 // Bootstrap
@@ -20,6 +20,12 @@ function L5() {
             <hr/>
           </Col>
         </Row>
+
+        <Row>
+				<Col>
+					<br />
+				</Col>
+			</Row>
 
         <Row>
           <Col className="opisLekcije">
@@ -45,15 +51,68 @@ function L5() {
           </Col>
         </Row>
 
-        <h2 className="text-center">Vježbe</h2>
+        <Row>
+				<Col>
+					<br />
+				</Col>
+			</Row>
 
+			<Row>
+				<Col>
+					<br />
+				</Col>
+			</Row>
+
+        <h2 className="text-center">Vježbe</h2>
         <hr/>
 
-        <Row className="text-center">
-          <Col>
-            VJEŽBE 5
-          </Col>
-        </Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj8')}
+				</Col>
+			</Row>
+
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj9')}
+				</Col>
+			</Row>
+
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj11')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj10')}
+				</Col>
+			</Row>
+
+			<Row className="text-center">
+				<Col>
+          ۞
+          {VjezbeRow(data, 'vjezba', 'broj14')}
+					۞
+          {VjezbeRow(data, 'vjezba', 'broj13')}
+          ۞
+					{VjezbeRow(data, 'vjezba', 'broj12')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+          {VjezbeRow(data, 'vjezba', 'broj16')}
+          ۞
+					{VjezbeRow(data, 'vjezba', 'broj15')}
+				</Col>
+			</Row>
+      <Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj17')}
+				</Col>
+			</Row>
 
         <Footer prev="/lekcija4" next="/lekcija6" />
 
