f65a2f1d845ed1abc10fcab04204bf49bcfde331
brodude456 <fatihefeememisevic@gmail.com>
2019-10-02 17:03:47 +0200

    vjezba 15

--- src/Lessons/Lesson15.js
+++ src/Lessons/Lesson15.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import data from '../Data/L15Data.json';
 import PlayerRow from '../Helpers/PlayerHelper';
-//import VjezbeRow from '../Helpers/VjezbeHelper';
+import VjezbeRow from '../Helpers/VjezbeHelper';
 import Footer from '../Body/MainFooter';
 
 // Bootstrap
@@ -20,6 +20,12 @@ function L15() {
             <hr/>
           </Col>
         </Row>
+
+        <Row>
+				<Col>
+					<br />
+				</Col>
+			</Row>  
 
         <Row>
           <Col className="opisLekcije">
@@ -63,17 +69,58 @@ function L15() {
           </Col>
         </Row>
 
-        <h2 className="text-center">Vježbe</h2>
+        <Row>
+				<Col>
+					<br />
+				</Col>
+			</Row>  
+      
+      <Row>
+				<Col>
+					<br />
+				</Col>
+			</Row>
 
+      <h2 className="text-center">Vježbe</h2>
         <hr/>
 
-        <Row className="text-center">
-          <Col>
-            VJEŽBE 15
-          </Col>
-        </Row>
+			<Row className="text-center">
+				<Col>
+					۞
+          {VjezbeRow(data, 'vjezba', 'broj13')}
+          ۞
+          {VjezbeRow(data, 'vjezba', 'broj12')}
+				</Col>
+			</Row>
 
-        <hr />
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj14')}
+				</Col>
+			</Row>
+
+			<Row className="text-center">
+				<Col>
+          ۞
+          {VjezbeRow(data, 'vjezba', 'broj16')}
+          ۞
+          {VjezbeRow(data, 'vjezba', 'broj15')}
+				</Col>
+			</Row>
+
+			<Row className="text-center">
+				<Col>
+					۞
+          {VjezbeRow(data, 'vjezba', 'broj17')}
+				</Col>
+			</Row>
+      <Row className="text-center">
+				<Col>
+					۞
+          {VjezbeRow(data, 'vjezba', 'broj18')}
+				</Col>
+			</Row>
 
         <Footer prev="/lekcija14" next="/lekcija16" />
 
