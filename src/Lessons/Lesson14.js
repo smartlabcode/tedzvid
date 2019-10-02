e04a524c3a1abf274787a299849071ab6a61bdeb
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2019-10-02 17:04:53 +0200

    ubaci 14. vjezbu

--- src/Lessons/Lesson14.js
+++ src/Lessons/Lesson14.js
@@ -1,79 +1,113 @@
 import React from 'react';
 import data from '../Data/L14Data.json';
 import PlayerRow from '../Helpers/PlayerHelper';
-//import VjezbeRow from '../Helpers/VjezbeHelper';
+import VjezbeRow from '../Helpers/VjezbeHelper';
 import Footer from '../Body/MainFooter';
 
 // Bootstrap
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 // Other
 import '../App.scss';
 
 function L14() {
-    return (
-        <React.Fragment>
-        <Row>
-          <Col>
-            <h2 className="text-center font-weight-bold text-uppercase">HUKMURRA</h2>
-            <h4 className="text-center">izgovor harfa R (ر)</h4>
-            <hr/>
-          </Col>
-        </Row>
+	return (
+		<React.Fragment>
+			<Row>
+				<Col>
+					<h2 className="text-center font-weight-bold text-uppercase">HUKMURRA</h2>
+					<h4 className="text-center">izgovor harfa R (ر)</h4>
+					<hr />
+				</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          Harf R (ر) se uči krupno u sljedećim situacijama:
-          </Col>
-        </Row>  
+			<Row>
+				<Col className="opisLekcije">Harf R (ر) se uči krupno u sljedećim situacijama:</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          <strong>1.</strong> kada je harf R sa vokalom E ( { PlayerRow(data, 'row1') }) ili vokalom U ( { PlayerRow(data, 'row2') }), npr.: { PlayerRow(data, 'row3') }
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					<strong>1.</strong> kada je harf R sa vokalom E ( {PlayerRow(data, 'row1')}) ili vokalom U ({' '}
+					{PlayerRow(data, 'row2')}), npr.: {PlayerRow(data, 'row3')}
+				</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          <strong>2.</strong> kada je harf R sa sukunom, a prije njeg harf sa vokalom E (رْ ــــَـــ) ili U (ــــُــ رْ), npr.:
-          { PlayerRow(data, 'row4') }
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					<strong>2.</strong> kada je harf R sa sukunom, a prije njeg harf sa vokalom E (رْ ــــَـــ) ili U
+					(ــــُــ رْ), npr.:
+					{PlayerRow(data, 'row4')}
+				</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          <strong>3.</strong> kada je harf R sa sukunom, prije njeg harf sa sukunom, a prije toga harf sa vokalom E (رْ ــــْـــ ــــَـــ) ili U (رْ ــــْـــ ــــُـــ), npr.: { PlayerRow(data, 'row5') }
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					<strong>3.</strong> kada je harf R sa sukunom, prije njeg harf sa sukunom, a prije toga harf sa
+					vokalom E (رْ ــــْـــ ــــَـــ) ili U (رْ ــــْـــ ــــُـــ), npr.: {PlayerRow(data, 'row5')}
+				</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          <strong>4.</strong> kada je harf R sa sukunom (رْ), a prije njeg glas sa nestalnom kesrom, npr.: { PlayerRow(data, 'row6') }
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					<strong>4.</strong> kada je harf R sa sukunom (رْ), a prije njeg glas sa nestalnom kesrom, npr.:{' '}
+					{PlayerRow(data, 'row6')}
+				</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          <strong>5.</strong> kada je harf R sa sukunom (رْ), a poslije njeg jedan od krupnih harfova, npr.: { PlayerRow(data, 'row7') }
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					<strong>5.</strong> kada je harf R sa sukunom (رْ), a poslije njeg jedan od krupnih harfova, npr.:{' '}
+					{PlayerRow(data, 'row7')}
+				</Col>
+			</Row>
+			<h2 className="text-center">Vježbe</h2>
+			<hr />
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj14')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj13')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj12')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj16')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj15')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj18')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj17')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj20')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj19')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj22')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj21')}
+				</Col>
+			</Row>
 
-        <h2 className="text-center">Vježbe</h2>
+			<Footer prev="/lekcija13" next="/lekcija14_2" />
+		</React.Fragment>
+	);
+}
 
-        <hr/>
-
-        <Row className="text-center">
-          <Col>
-            VJEŽBE 14
-          </Col>
-        </Row>
-
-        <hr />
-
-        <Footer prev="/lekcija13" next="/lekcija14_2" />
-
-        </React.Fragment>
-    );
-  }
-
-  export default L14;
+export default L14;
--- src/Lessons/Lesson14_2.js
+++ src/Lessons/Lesson14_2.js
@@ -1,65 +1,106 @@
 import React from 'react';
 import data from '../Data/L14Data.json';
 import PlayerRow from '../Helpers/PlayerHelper';
-//import VjezbeRow from '../Helpers/VjezbeHelper';
+import VjezbeRow from '../Helpers/VjezbeHelper';
 import Footer from '../Body/MainFooter';
 
 // Bootstrap
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 // Other
 import '../App.scss';
 
 function L14_2() {
-    return (
-        <React.Fragment>
-        <Row>
-          <Col className="opisLekcije">
-          Harf R (ر) se uči tanko u sljedećim situacijama:
-          </Col>
-        </Row>  
+	return (
+		<React.Fragment>
+			<Row>
+				<Col className="opisLekcije">Harf R (ر) se uči tanko u sljedećim situacijama:</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          <strong>1.</strong>	kada je harf R sa vokalom I ( { PlayerRow(data, 'row9') } ), npr.: { PlayerRow(data, 'row8') }
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					<strong>1.</strong> kada je harf R sa vokalom I ( {PlayerRow(data, 'row9')} ), npr.:{' '}
+					{PlayerRow(data, 'row8')}
+				</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          <strong>2.</strong>	kada je harf R sa sukunom, a prije njeg harf sa vokalom I (ــــِـــ رْ), npr.: { PlayerRow(data, 'row10') }
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					<strong>2.</strong> kada je harf R sa sukunom, a prije njeg harf sa vokalom I (ــــِـــ رْ), npr.:{' '}
+					{PlayerRow(data, 'row10')}
+				</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          <strong>3.</strong>	kada je harf R sa sukunom, prije njeg harf sa sukunom, a prije toga harf sa vokalom I (رْ ــــِـــ ــــْـــ), npr.: 
-          { PlayerRow(data, 'row11') }
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					<strong>3.</strong> kada je harf R sa sukunom, prije njeg harf sa sukunom, a prije toga harf sa
+					vokalom I (رْ ــــِـــ ــــْـــ), npr.:
+					{PlayerRow(data, 'row11')}
+				</Col>
+			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-          <strong>4.</strong>	4.	kada je harf R sa sukunom, a prije njeg harf J također sa sukunom, a prije toga harf sa vokalom E (ــــَـــ يْـــرْ), npr.: { PlayerRow(data, 'row12') }
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					<strong>4.</strong> 4. kada je harf R sa sukunom, a prije njeg harf J također sa sukunom, a prije
+					toga harf sa vokalom E (ــــَـــ يْـــرْ), npr.: {PlayerRow(data, 'row12')}
+				</Col>
+			</Row>
 
-        <h2 className="text-center">Vježbe</h2>
+			<h2 className="text-center">Vježbe</h2>
+			<hr />
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj29')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj28')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj31')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj30')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj33')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj32')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj35')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj34')}
+				</Col>
+			</Row>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj38')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj37')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj36')}
+				</Col>
+			</Row>
 
-        <hr/>
+			<Row className="text-center">
+				<Col>
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj39')}
+				</Col>
+			</Row>
 
-        <Row className="text-center">
-          <Col>
-            VJEŽBE 14_2
-          </Col>
-        </Row>
+			<Footer prev="/lekcija14" next="/lekcija15" />
+		</React.Fragment>
+	);
+}
 
-        <hr />
-
-        <Footer prev="/lekcija14" next="/lekcija15" />
-
-        </React.Fragment>
-    );
-  }
-
-  export default L14_2;
+export default L14_2;
