d442934a1bdaf313bd23b3f6051da7244b418d88
brodude456 <fatihefeememisevic@gmail.com>
2019-10-03 12:44:04 +0200

    lekcija 6 dodano arapske spanove

--- src/Lessons/Lesson6.js
+++ src/Lessons/Lesson6.js
@@ -64,9 +64,9 @@ function L6() {
 
 			<Row>
 				<Col className="opisLekcije">
-					Kada poslije harfa N sa sukunom (نْ) ili tenvina EN ــــــًــــــ, IN ـــــٍــــــ , UN ــــــٌـــــ
-					dođe jedan od četiri harfa: <Arabic bold={true}>ي م ن و</Arabic> (sadržana u riječi jemnu –
-					يَمْنُو), dolazi do uklapanja harfa N (ن) u jedan od spomenuta četiri harfa, propuštajući zrak kroz
+					Kada poslije harfa N sa <strong>sukunom</strong> <span className="arapski-lekcija">(نْ)</span> ili <strong>tenvina</strong> EN<span className="arapski-lekcija">ــــــًــــــ</span> , IN<span className="arapski-lekcija">ـــــٍــــــ</span>  ,UN <span className="arapski-lekcija">ــــــٌـــــ</span>  
+					dođe jedan od četiri harfa: <Arabic bold={true}>ي م ن و</Arabic> (sadržana u riječi <strong>jemnu</strong> –
+					<span className="arapski-lekcija" style={{ fontSize: '2em' }}>يَمْنُو</span>), dolazi do uklapanja harfa <strong>N(<span className="arapski-lekcija" style={{ fontSize: '2em' }}>ن</span>)</strong>  u jedan od spomenuta četiri harfa, propuštajući zrak kroz
 					nos u trajanju od 2 hareketa, npr.:
 				</Col>
 			</Row>
