85045c1c29fd88e9454647f928f10b7f03dd5d2f
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2019-10-03 12:44:57 +0200

    ubaci korekcije za petu lekciju

--- src/Lessons/Lesson5.js
+++ src/Lessons/Lesson5.js
@@ -5,53 +5,51 @@ import VjezbeRow from '../Helpers/VjezbeHelper';
 import Footer from '../Body/MainFooter';
 
 // Bootstrap
-import {Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 // Other
 import '../App.scss';
 
 function L5() {
-    return (
-        <React.Fragment>
-        <Row>
-          <Col>
-            <h2 className="text-center font-weight-bold text-uppercase">IDGAM MISLEJN MEAL-GUNNEH</h2>
-            <h4 className="text-center">uklapanje istih harfova sa propuštanjem zraka kroz nos</h4>
-            <hr/>
-          </Col>
-        </Row>
+	return (
+		<React.Fragment>
+			<Row>
+				<Col>
+					<h2 className="text-center font-weight-bold text-uppercase">IDGAM MISLEJN MEAL-GUNNEH</h2>
+					<h4 className="text-center">uklapanje istih harfova sa propuštanjem zraka kroz nos</h4>
+					<hr />
+				</Col>
+			</Row>
 
-        <Row>
+			<Row>
 				<Col>
 					<br />
 				</Col>
 			</Row>
 
-        <Row>
-          <Col className="opisLekcije">
-            Kada poslije harfa  M (م) sa sukunom dođe harf M (م) sa hareketom, dolazi do uklapanja harfa M (م) sa sukunom u harf M (م) sa hareketom, uz propuštanje zraka kroz nos u trajanju od 2 hareketa, npr.:
-          </Col>
-        </Row>
+			<Row>
+				<Col className="opisLekcije">
+					Kada poslije harfa M <span className="arapski-lekcija">(م)</span> sa sukunom dođe harf M{' '}
+					<span className="arapski-lekcija">(م) </span>sa hareketom, dolazi do uklapanja harfa M{' '}
+					<span className="arapski-lekcija">(م)</span> sa sukunom u harf M{' '}
+					<span className="arapski-lekcija">(م)</span> sa hareketom, uz propuštanje zraka kroz nos u trajanju
+					od 2 hareketa, npr.:
+				</Col>
+			</Row>
 
-        <Row className="text-center">
-          <Col>
-            { PlayerRow(data, 'row1') }
-          </Col>
-        </Row>
+			<Row className="text-center">
+				<Col>{PlayerRow(data, 'row1')}</Col>
+			</Row>
 
-        <Row className="text-center">
-          <Col>
-            { PlayerRow(data, 'row2') }
-          </Col>
-        </Row>
+			<Row className="text-center">
+				<Col>{PlayerRow(data, 'row2')}</Col>
+			</Row>
 
-        <Row className="text-center">
-          <Col>
-            { PlayerRow(data, 'row3') }
-          </Col>
-        </Row>
+			<Row className="text-center">
+				<Col>{PlayerRow(data, 'row3')}</Col>
+			</Row>
 
-        <Row>
+			<Row>
 				<Col>
 					<br />
 				</Col>
@@ -63,8 +61,8 @@ function L5() {
 				</Col>
 			</Row>
 
-        <h2 className="text-center">Vježbe</h2>
-        <hr/>
+			<h2 className="text-center">Vježbe</h2>
+			<hr />
 
 			<Row className="text-center">
 				<Col>
@@ -91,33 +89,32 @@ function L5() {
 
 			<Row className="text-center">
 				<Col>
-          ۞
-          {VjezbeRow(data, 'vjezba', 'broj14')}
 					۞
-          {VjezbeRow(data, 'vjezba', 'broj13')}
-          ۞
+					{VjezbeRow(data, 'vjezba', 'broj14')}
+					۞
+					{VjezbeRow(data, 'vjezba', 'broj13')}
+					۞
 					{VjezbeRow(data, 'vjezba', 'broj12')}
 				</Col>
 			</Row>
 			<Row className="text-center">
 				<Col>
 					۞
-          {VjezbeRow(data, 'vjezba', 'broj16')}
-          ۞
+					{VjezbeRow(data, 'vjezba', 'broj16')}
+					۞
 					{VjezbeRow(data, 'vjezba', 'broj15')}
 				</Col>
 			</Row>
-      <Row className="text-center">
+			<Row className="text-center">
 				<Col>
 					۞
 					{VjezbeRow(data, 'vjezba', 'broj17')}
 				</Col>
 			</Row>
 
-        <Footer prev="/lekcija4" next="/lekcija6" />
+			<Footer prev="/lekcija4" next="/lekcija6" />
+		</React.Fragment>
+	);
+}
 
-        </React.Fragment>
-    );
-  }
-
-  export default L5;
+export default L5;
