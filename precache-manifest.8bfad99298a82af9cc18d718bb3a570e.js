a49e617620908e1295dcb951c4aa974dbc8e9b9a
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2020-07-01 12:09:28 +0200

    Updates

---
+++ precache-manifest.8bfad99298a82af9cc18d718bb3a570e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032300e4ad83df24cc4540fafdaedc23",
+    "url": "/tedzvid/index.html"
+  },
+  {
+    "revision": "1b0f7b92bd08100433ff",
+    "url": "/tedzvid/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "5f2e093d6efede5957ec",
+    "url": "/tedzvid/static/css/main.d27caba8.chunk.css"
+  },
+  {
+    "revision": "1b0f7b92bd08100433ff",
+    "url": "/tedzvid/static/js/2.d619f994.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tedzvid/static/js/2.d619f994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2e093d6efede5957ec",
+    "url": "/tedzvid/static/js/main.5e2bddf6.chunk.js"
+  },
+  {
+    "revision": "fd2260fa53914c8d5ce7",
+    "url": "/tedzvid/static/js/runtime-main.d772a02c.js"
+  },
+  {
+    "revision": "c5109a1c3ff7281895bc99f534c12ebd",
+    "url": "/tedzvid/static/media/KuranKerimFontHamdullah.c5109a1c.ttf"
+  }
+]);
