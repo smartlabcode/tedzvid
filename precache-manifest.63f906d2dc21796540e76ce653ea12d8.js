d4ab48ddddb6e6e5f9229f195f52e43f42dbb847
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2020-07-01 23:26:25 +0200

    Updates

---
+++ precache-manifest.63f906d2dc21796540e76ce653ea12d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d230714bbb1d5e1f0a4411aec607de1d",
+    "url": "/tedzvid/index.html"
+  },
+  {
+    "revision": "1b0f7b92bd08100433ff",
+    "url": "/tedzvid/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "58e0563e270bb07ea95e",
+    "url": "/tedzvid/static/css/main.ea9ccb80.chunk.css"
+  },
+  {
+    "revision": "1b0f7b92bd08100433ff",
+    "url": "/tedzvid/static/js/2.d619f994.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tedzvid/static/js/2.d619f994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e0563e270bb07ea95e",
+    "url": "/tedzvid/static/js/main.8b559fdf.chunk.js"
+  },
+  {
+    "revision": "fd2260fa53914c8d5ce7",
+    "url": "/tedzvid/static/js/runtime-main.d772a02c.js"
+  },
+  {
+    "revision": "c5109a1c3ff7281895bc99f534c12ebd",
+    "url": "/tedzvid/static/media/KuranKerimFontHamdullah.c5109a1c.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.966984eab609f0dda7c742f71597e1a9.js"
+  "/tedzvid/precache-manifest.63f906d2dc21796540e76ce653ea12d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tedzvid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
