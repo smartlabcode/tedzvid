248356e556678821ef3e1d2eeeb4317325169bcb
Fahrudin Halilovic <fahrudin.halilovic@gmail.com>
2020-07-01 12:09:01 +0200

    update homepage in package.json

--- src/App.js
+++ src/App.js
@@ -35,31 +35,31 @@ import { BrowserRouter, Route } from 'react-router-dom';
 function App() {
 	return (
 		<BrowserRouter basename={process.env.PUBLIC_URL}>
-				<Route path="/" exact component={Demo} />
-				<Route path="/lekcije" exact component={Home} />
-				<Route path="/lekcija1" component={L1} />
-				<Route path="/lekcija2" component={L2} />
-				<Route path="/lekcija3" component={L3} />
-				<Route path="/lekcija4" component={L4} />
-				<Route path="/lekcija5" component={L5} />
-				<Route path="/lekcija6" component={L6} />
-				<Route path="/lekcija7" component={L7} />
-				<Route path="/lekcija8" component={L8} />
-				<Route path="/lekcija9" component={L9} />
-				<Route path="/lekcija10" component={L10} />
-				<Route path="/lekcija11" component={L11} />
-				<Route path="/lekcija12" component={L12} />
-				<Route path="/lekcija13" component={L13} />
-				<Route path="/lekcija14" component={L14} />
-				<Route path="/lekcija14_2" component={L14_2} />
-				<Route path="/lekcija15" component={L15} />
-				<Route path="/lekcija16" component={L16} />
-				<Route path="/lekcija17" component={L17} />
-				<Route path="/lekcija18" component={L18} />
-				<Route path="/lekcija19" component={L19} />
-				<Route path="/lekcija20" component={L20} />
-				<Route path="/lekcija21" component={L21} />
-				<Route path="/lekcija22" component={L22} />
+			<Route path="/" exact component={Demo} />
+			<Route path="/lekcije" exact component={Home} />
+			<Route path="/lekcija1" component={L1} />
+			<Route path="/lekcija2" component={L2} />
+			<Route path="/lekcija3" component={L3} />
+			<Route path="/lekcija4" component={L4} />
+			<Route path="/lekcija5" component={L5} />
+			<Route path="/lekcija6" component={L6} />
+			<Route path="/lekcija7" component={L7} />
+			<Route path="/lekcija8" component={L8} />
+			<Route path="/lekcija9" component={L9} />
+			<Route path="/lekcija10" component={L10} />
+			<Route path="/lekcija11" component={L11} />
+			<Route path="/lekcija12" component={L12} />
+			<Route path="/lekcija13" component={L13} />
+			<Route path="/lekcija14" component={L14} />
+			<Route path="/lekcija14_2" component={L14_2} />
+			<Route path="/lekcija15" component={L15} />
+			<Route path="/lekcija16" component={L16} />
+			<Route path="/lekcija17" component={L17} />
+			<Route path="/lekcija18" component={L18} />
+			<Route path="/lekcija19" component={L19} />
+			<Route path="/lekcija20" component={L20} />
+			<Route path="/lekcija21" component={L21} />
+			<Route path="/lekcija22" component={L22} />
 		</BrowserRouter>
 	);
 }
