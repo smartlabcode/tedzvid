2c355eb13a9be6becf4f7808e4df23a011d622dd
Vejsil Hrustic <bakerundo@gmail.com>
2023-10-29 21:15:02 +0100

    Ispravke uglavnom na engleskoj verziji

--- src/Components/Lesson10_en.jsx
+++ src/Components/Lesson10_en.jsx
@@ -36,284 +36,303 @@ const Lesson10_en = () => {
 	const { i18n } = useTranslation();
   return (
     <>
-<LekcijaMenu broj='10' naziv='IZHAAR SHAFAWI' />
-			<Container>
-				<Row>
-					<Col>
-						<div className='mobileTop'>
-							<center>
-								<img
-									src={process.env.PUBLIC_URL + '/assets/svg/Group 61.svg'}
-									alt='Group 61'
-								/>
-							</center>
-							<h2
-								className='text-center font-weight-bold text-uppercase'
-								id='lekcija'>
-								10 IZHAAR SHAFAWI
-							</h2>
-						</div>
-						<h4 className='text-center'>
-							<strong>Clear pronunciation of the letter M (م)</strong>
-						</h4>
-						<hr />
-					</Col>
-				</Row>
+      <LekcijaMenu broj="10" naziv="IZHAAR SHAFAWI" />
+      <Container>
+        <Row>
+          <Col>
+            <div className="mobileTop">
+              <center>
+                <img
+                  src={process.env.PUBLIC_URL + "/assets/svg/Group 61.svg"}
+                  alt="Group 61"
+                />
+              </center>
+              <h2
+                className="text-center font-weight-bold text-uppercase"
+                id="lekcija"
+              >
+                10 IZHAAR SHAFAWI
+              </h2>
+            </div>
+            <h4 className="text-center">
+              <strong>Clear pronunciation of the letter M (م)</strong>
+            </h4>
+            <hr />
+          </Col>
+        </Row>
 
-				<Row>
-					<Col>
-						<br />
-					</Col>
-				</Row>
-				<IconContext.Provider
-					value={{ size: '30px', style: { float: 'right' } }}>
-					<MdZoomOutMap className='zoomIcon' onClick={handleShowL} />
-				</IconContext.Provider>
-				<Row>
-					<Col className='opisLekcije'>
-						When the letter M with <strong>sukoon</strong> (مْ) precedes any letter, except for the letters <strong>B</strong> (ب) and <strong>M</strong> (م), then the letter <strong>M</strong> (م) is pronounced <u>clearly</u>, without assimilation. For example:
-					</Col>
-				</Row>
+        <Row>
+          <Col>
+            <br />
+          </Col>
+        </Row>
+        <IconContext.Provider
+          value={{ size: "30px", style: { float: "right" } }}
+        >
+          <MdZoomOutMap className="zoomIcon" onClick={handleShowL} />
+        </IconContext.Provider>
+        <Row>
+          <Col className="opisLekcije">
+            When the letter <strong>M</strong> with <strong>sukoon</strong> (
+            <span className="arapski-lekcija">مْ</span>) precedes{" "}
+            <u>any letter</u>, <u>except</u> for the letters <strong>B</strong>{" "}
+            (<span className="arapski-lekcija">ب</span>) and <strong>M</strong>{" "}
+            (<span className="arapski-lekcija">م</span>), then the letter{" "}
+            <strong>M</strong> (<span className="arapski-lekcija">م</span>) is
+            pronounced <u>clearly</u>, without assimilation. For example:
+          </Col>
+        </Row>
 
-				<Row>
-					<Col>
-						<br />
-					</Col>
-				</Row>
+        <Row>
+          <Col>
+            <br />
+          </Col>
+        </Row>
 
-				<Row className='text-center  rtl'>
-					<Col>
-						{PlayerRow(data, 'row1')}
-						{VjezbeRow(data, 'multirow', 'word1')}
-					</Col>
-				</Row>
-				<Row className='text-center  reorder-basic rtl'>
-					<Col>{VjezbeRow(data, 'multirow', 'word2')}</Col>
-				</Row>
-				<Row className='text-center  rtl'>
-					<Col>{VjezbeRow(data, 'multirow', 'word3')}</Col>
-				</Row>
+        <Row className="text-center  rtl">
+          <Col>
+            {PlayerRow(data, "row1")}
+            {VjezbeRow(data, "multirow", "word1")}
+          </Col>
+        </Row>
+        <Row className="text-center  reorder-basic rtl">
+          <Col>{VjezbeRow(data, "multirow", "word2")}</Col>
+        </Row>
+        <Row className="text-center  rtl">
+          <Col>{VjezbeRow(data, "multirow", "word3")}</Col>
+        </Row>
 
-				<Row>
-					<Col>
-						<br />
-					</Col>
-				</Row>
+        <Row>
+          <Col>
+            <br />
+          </Col>
+        </Row>
 
-				<Row>
-					<Col>
-						<br />
-					</Col>
-				</Row>
-				<Modal
-					show={showL}
-					onHide={handleCloseL}
-					backdrop='static'
-					keyboard={false}>
-					<Modal.Header closeButton>
-						<Modal.Title>LESSON</Modal.Title>
-					</Modal.Header>
-					<Modal.Body className='custom-modal'>
-						<Row>
-							<Col className='opisLekcije'>
-						When the letter M with <strong>sukoon</strong> (مْ) precedes any letter, except for the letters <strong>B</strong> (ب) and <strong>M</strong> (م), then the letter <strong>M</strong> (م) is pronounced <u>clearly</u>, without assimilation. For example:
-					</Col>
-						</Row>
+        <Row>
+          <Col>
+            <br />
+          </Col>
+        </Row>
+        <Modal
+          show={showL}
+          onHide={handleCloseL}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>LESSON</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="custom-modal">
+            <Row>
+              <Col className="opisLekcije">
+                When the letter <strong>M</strong> with <strong>sukoon</strong>{" "}
+                (<span className="arapski-lekcija">مْ</span>) precedes{" "}
+                <u>any letter</u>, <u>except</u> for the letters{" "}
+                <strong>B</strong> (<span className="arapski-lekcija">ب</span>)
+                and <strong>M</strong> (
+                <span className="arapski-lekcija">م</span>), then the letter{" "}
+                <strong>M</strong> (<span className="arapski-lekcija">م</span>)
+                is pronounced <u>clearly</u>, without assimilation. For example:
+              </Col>
+            </Row>
 
-						<Row>
-							<Col>
-								<br />
-							</Col>
-						</Row>
+            <Row>
+              <Col>
+                <br />
+              </Col>
+            </Row>
 
-						<Row className='text-center reorder rtl'>
-							<Col>
-								{PlayerRow(data, 'row1')}
-								{VjezbeRow(data, 'multirow', 'word1')}
-							</Col>
-						</Row>
-						<Row className='text-center reorder rtl'>
-							<Col>{VjezbeRow(data, 'multirow', 'word2')}</Col>
-						</Row>
-						<Row className='text-center reorder rtl'>
-							<Col>{VjezbeRow(data, 'multirow', 'word3')}</Col>
-						</Row>
-						<Row>
-							<Col>
-								<br />
-							</Col>
-						</Row>
+            <Row className="text-center reorder rtl">
+              <Col>
+                {PlayerRow(data, "row1")}
+                {VjezbeRow(data, "multirow", "word1")}
+              </Col>
+            </Row>
+            <Row className="text-center reorder rtl">
+              <Col>{VjezbeRow(data, "multirow", "word2")}</Col>
+            </Row>
+            <Row className="text-center reorder rtl">
+              <Col>{VjezbeRow(data, "multirow", "word3")}</Col>
+            </Row>
+            <Row>
+              <Col>
+                <br />
+              </Col>
+            </Row>
 
-						<Row>
-							<Col>
-								<br />
-							</Col>
-						</Row>
-					</Modal.Body>
-					<Modal.Footer>
-						<Button variant='secondary' onClick={handleCloseL}>
-							Close
-						</Button>
-					</Modal.Footer>
-				</Modal>
-				<h2 className='text-center' id='vjezba'>
-					<strong>PRACTICE</strong>
-				</h2>
-				<hr />
-				<Row>
-					<Col>
-						<br />
-					</Col>
-				</Row>
-				<IconContext.Provider
-					value={{ size: '30px', style: { float: 'right' } }}>
-					<MdZoomOutMap className='zoomIcon' onClick={handleShow} />
-				</IconContext.Provider>
+            <Row>
+              <Col>
+                <br />
+              </Col>
+            </Row>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseL}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <h2 className="text-center" id="vjezba">
+          <strong>PRACTICE</strong>
+        </h2>
+        <hr />
+        <Row>
+          <Col>
+            <br />
+          </Col>
+        </Row>
+        <IconContext.Provider
+          value={{ size: "30px", style: { float: "right" } }}
+        >
+          <MdZoomOutMap className="zoomIcon" onClick={handleShow} />
+        </IconContext.Provider>
 
-				<Row className='text-center'>
-					<Col className='mobile-row'>
-						<span className='tacka'>
-							{VjezbeRow(data, 'vjezba', 'broj6')} ۞
-						</span>
-						<span className='tacka'>
-							{VjezbeRow(data, 'vjezba', 'broj7')} ۞
-						</span>
-						<span className='tacka'>
-							{VjezbeRow(data, 'vjezba', 'broj8')} ۞
-						</span>
-					</Col>
-				</Row>
-				<Row className='text-center'>
-					<Col className='mobile-row'>
-						<span className='tacka'>
-							{VjezbeRow(data, 'vjezba', 'broj9')} ۞
-						</span>
-						<span className='tacka'>
-							{VjezbeRow(data, 'vjezba', 'broj10')} ۞
-						</span>
-					</Col>
-				</Row>
-				<Row className='text-center'>
-					<Col className='mobile-row'>
-						<span className='tacka'>
-							{VjezbeRow(data, 'vjezba', 'broj11')} ۞
-						</span>
-					</Col>
-				</Row>
-				<Row className='text-center'>
-					<Col className='mobile-row'>
-						<span className='tacka'>
-							{VjezbeRow(data, 'vjezba', 'broj12')} ۞
-						</span>
-					</Col>
-				</Row>
-				<Row className='text-center'>
-					<Col className='mobile-row'>
-						<span className='tacka'>
-							{VjezbeRow(data, 'vjezba', 'broj13')} ۞
-						</span>
-					</Col>
-				</Row>
-				<Row className='text-center'>
-					<Col className='mobile-row'>
-						<span className='tacka'>
-							{VjezbeRow(data, 'vjezba', 'broj14')} ۞
-						</span>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<hr />
-						<h2 className='text-center' id='video'>
-							<strong>VIDEO LECTURE</strong>
-						</h2>
-						<center>
-							<div className='video'>
-								<iframe
-									width='900'
-									height='506'
-									src='https://www.youtube.com/embed/8Wtv5ieYWy0?list=PLcdhKKk9LmetUjroRtBCCkugO_whS-cW2'
-									title='YouTube video player'
-									frameborder='0'
-									allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-									allowFullScreen='true'
-									webkitallowfullscreen='true'
-									mozallowfullscreen='true'></iframe>
-							</div>
-						</center>
-					</Col>
-				</Row>
-				<Modal
-					show={show}
-					onHide={handleClose}
-					backdrop='static'
-					keyboard={false}>
-					<Modal.Header closeButton>
-						<Modal.Title>PRACTICE</Modal.Title>
-					</Modal.Header>
-					<Modal.Body className='custom-modal'>
-						<Row className='text-center'>
-							<Col className='mobile-row'>
-								<span className='tacka'>
-									{VjezbeRow(data, 'vjezba', 'broj6')} ۞
-								</span>
-								<span className='tacka'>
-									{VjezbeRow(data, 'vjezba', 'broj7')} ۞
-								</span>
-								<span className='tacka'>
-									{VjezbeRow(data, 'vjezba', 'broj8')} ۞
-								</span>
-							</Col>
-						</Row>
-						<Row className='text-center'>
-							<Col className='mobile-row'>
-								<span className='tacka'>
-									{VjezbeRow(data, 'vjezba', 'broj9')} ۞
-								</span>
-								<span className='tacka'>
-									{VjezbeRow(data, 'vjezba', 'broj10')} ۞
-								</span>
-							</Col>
-						</Row>
-						<Row className='text-center'>
-							<Col className='mobile-row'>
-								<span className='tacka'>
-									{VjezbeRow(data, 'vjezba', 'broj11')} ۞
-								</span>
-							</Col>
-						</Row>
-						<Row className='text-center'>
-							<Col className='mobile-row'>
-								<span className='tacka'>
-									{VjezbeRow(data, 'vjezba', 'broj12')} ۞
-								</span>
-							</Col>
-						</Row>
-						<Row className='text-center'>
-							<Col className='mobile-row'>
-								<span className='tacka'>
-									{VjezbeRow(data, 'vjezba', 'broj13')} ۞
-								</span>
-							</Col>
-						</Row>
-						<Row className='text-center'>
-							<Col className='mobile-row'>
-								<span className='tacka'>
-									{VjezbeRow(data, 'vjezba', 'broj14')} ۞
-								</span>
-							</Col>
-						</Row>
-					</Modal.Body>
-					<Modal.Footer>
-						<Button variant='secondary' onClick={handleClose}>
-							Close
-						</Button>
-					</Modal.Footer>
-				</Modal>
-				<Footer prev='/lekcija9' next='/lekcija11' />
-			</Container>
+        <Row className="text-center">
+          <Col className="mobile-row">
+            <span className="tacka">
+              {VjezbeRow(data, "vjezba", "broj6")} ۞
+            </span>
+            <span className="tacka">
+              {VjezbeRow(data, "vjezba", "broj7")} ۞
+            </span>
+            <span className="tacka">
+              {VjezbeRow(data, "vjezba", "broj8")} ۞
+            </span>
+          </Col>
+        </Row>
+        <Row className="text-center">
+          <Col className="mobile-row">
+            <span className="tacka">
+              {VjezbeRow(data, "vjezba", "broj9")} ۞
+            </span>
+            <span className="tacka">
+              {VjezbeRow(data, "vjezba", "broj10")} ۞
+            </span>
+          </Col>
+        </Row>
+        <Row className="text-center">
+          <Col className="mobile-row">
+            <span className="tacka">
+              {VjezbeRow(data, "vjezba", "broj11")} ۞
+            </span>
+          </Col>
+        </Row>
+        <Row className="text-center">
+          <Col className="mobile-row">
+            <span className="tacka">
+              {VjezbeRow(data, "vjezba", "broj12")} ۞
+            </span>
+          </Col>
+        </Row>
+        <Row className="text-center">
+          <Col className="mobile-row">
+            <span className="tacka">
+              {VjezbeRow(data, "vjezba", "broj13")} ۞
+            </span>
+          </Col>
+        </Row>
+        <Row className="text-center">
+          <Col className="mobile-row">
+            <span className="tacka">
+              {VjezbeRow(data, "vjezba", "broj14")} ۞
+            </span>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <hr />
+            <h2 className="text-center" id="video">
+              <strong>VIDEO LECTURE</strong>
+            </h2>
+            <center>
+              <div className="video">
+                <iframe
+                  width="900"
+                  height="506"
+                  src="https://www.youtube.com/embed/8Wtv5ieYWy0?list=PLcdhKKk9LmetUjroRtBCCkugO_whS-cW2"
+                  title="YouTube video player"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen="true"
+                  webkitallowfullscreen="true"
+                  mozallowfullscreen="true"
+                ></iframe>
+              </div>
+            </center>
+          </Col>
+        </Row>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>PRACTICE</Modal.Title>
+          </Modal.Header>
+          <Modal.Body className="custom-modal">
+            <Row className="text-center">
+              <Col className="mobile-row">
+                <span className="tacka">
+                  {VjezbeRow(data, "vjezba", "broj6")} ۞
+                </span>
+                <span className="tacka">
+                  {VjezbeRow(data, "vjezba", "broj7")} ۞
+                </span>
+                <span className="tacka">
+                  {VjezbeRow(data, "vjezba", "broj8")} ۞
+                </span>
+              </Col>
+            </Row>
+            <Row className="text-center">
+              <Col className="mobile-row">
+                <span className="tacka">
+                  {VjezbeRow(data, "vjezba", "broj9")} ۞
+                </span>
+                <span className="tacka">
+                  {VjezbeRow(data, "vjezba", "broj10")} ۞
+                </span>
+              </Col>
+            </Row>
+            <Row className="text-center">
+              <Col className="mobile-row">
+                <span className="tacka">
+                  {VjezbeRow(data, "vjezba", "broj11")} ۞
+                </span>
+              </Col>
+            </Row>
+            <Row className="text-center">
+              <Col className="mobile-row">
+                <span className="tacka">
+                  {VjezbeRow(data, "vjezba", "broj12")} ۞
+                </span>
+              </Col>
+            </Row>
+            <Row className="text-center">
+              <Col className="mobile-row">
+                <span className="tacka">
+                  {VjezbeRow(data, "vjezba", "broj13")} ۞
+                </span>
+              </Col>
+            </Row>
+            <Row className="text-center">
+              <Col className="mobile-row">
+                <span className="tacka">
+                  {VjezbeRow(data, "vjezba", "broj14")} ۞
+                </span>
+              </Col>
+            </Row>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        <Footer prev="/lekcija9" next="/lekcija11" />
+      </Container>
     </>
-  )
+  );
 }
 
 export default Lesson10_en
